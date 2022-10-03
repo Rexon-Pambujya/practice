e35e295610c5e47e4d8dadefaa926a20bb7fcb24
Rexon <rexonpambujya2001@gmail.com>
2022-10-03 20:06:50 +0530

    commiting file

---
+++ src/Evenanodd.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Evenanodd {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int number = in.nextInt();
+        if(number % 2 == 0){
+            System.out.println(" The given Number is even");
+        } else {
+            System.out.println("Odd");
+
+        }
+    }
+}
